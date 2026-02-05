e538aa8a8a9c9ceab66122e6acca391c9cf10f52
Luca Eugenio Belaunzarán Consejo <lucabelaunzaran@gmail.com>
2026-02-05 22:47:02 +0100

    commit before uploading to remote repo

---
+++ examples/unsupervisedLearning/unsupervisedLearning.py
@@ -0,0 +1,26 @@
+import torch
+from torch import nn 
+
+def dictToTorch(data): #
+
+
+class CNN(nn.Module):
+    def __init__(self, )
+
+def declareSignals():
+    return [
+        {'name': 'targetLift', 'dType': 'DOB', 'sType': 'IN'},
+        {'name': 'realLift', 'dType': 'DOB', 'sType': 'IN'},
+        {'name': 'realGeometry', 'dType': 'DOB', 'sType': 'IN'},
+        {'name': 'targetGeometry', 'dType': 'DOB', 'sType': 'OUT'}
+    ]
+
+
+def init(inputs, **kwargs):
+    outputs = {}
+    return outputs
+
+
+def step(inputs, t, dt, **kwargs):
+    outputs = {}
+    return outputs
